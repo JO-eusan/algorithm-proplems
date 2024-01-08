c27c83011a6283c119c42ba066dea573e10ef652
JO-eusan <105639473+JO-eusan@users.noreply.github.com>
2024-01-09 02:42:50 +0900

    [Bronze V] Title: A-B, Time: 208 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        
+        int result = A - B;
+
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
