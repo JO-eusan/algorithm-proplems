c66655f7ac941edcde71922504985b5226304808
JO-eusan <105639473+JO-eusan@users.noreply.github.com>
2024-01-31 19:51:13 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 204 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main 
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+    
+        String S = scanner.nextLine();
+
+        System.out.print(S.length());
+
+        scanner.close();
+    }
+}
