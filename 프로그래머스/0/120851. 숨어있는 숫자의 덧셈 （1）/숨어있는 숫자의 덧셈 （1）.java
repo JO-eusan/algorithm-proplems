55ed270c5f580e068bbfec25573a3f1cc2b023c7
San <105639473+JO-eusan@users.noreply.github.com>
2024-07-03 01:11:54 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.02 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        
+        for(int i=0; i<my_string.length(); i++){
+            switch(my_string.charAt(i)){
+                case '1': answer+=1; break;
+                case '2': answer+=2; break;
+                case '3': answer+=3; break;
+                case '4': answer+=4; break;
+                case '5': answer+=5; break;
+                case '6': answer+=6; break;
+                case '7': answer+=7; break;
+                case '8': answer+=8; break;
+                case '9': answer+=9; break;
+            }
+        }
+        return answer;
+    }
+}
