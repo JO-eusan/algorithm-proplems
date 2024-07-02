80a16276e63d827bda68e81fa1914875d32695b6
San <105639473+JO-eusan@users.noreply.github.com>
2024-07-03 02:14:52 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.05 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i=1; i<=Math.sqrt(n); i++){
+            if(n%i == 0)
+                answer += 2;
+            
+            if(i == Math.sqrt(n))
+                answer -= 1;
+        }
+        
+        return answer;
+    }
+}
