44009ea6a15283be456643928deeb5e51af0215b
San <105639473+JO-eusan@users.noreply.github.com>
2024-07-09 18:14:37 +0900

    [level 2] Title: 전화번호 목록, Time: 379.99 ms, Memory: 203 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        
+        HashSet<String> set = new HashSet<>();
+        for(int i=0; i<phone_book.length; i++)
+            set.add(phone_book[i]);
+        
+        /* search */
+        for(int i=0; i<phone_book.length; i++) {
+            for(int j=0; j<phone_book[i].length(); j++) {
+                if(set.contains(phone_book[i].substring(0, j)))
+                    answer = false;
+            }
+        }
+        
+        return answer;
+    }
+}
