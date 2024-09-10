dd244c14733d8607b2c613f36769b42650715589
San <goodsan0330@gmail.com>
2024-09-10 15:39:19 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 136 ms, Memory: 17992 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -1,23 +1,11 @@
 import java.io.*;
 
-public class Main{
-
-    public static int recursion(String s, int l, int r){
-        count++;
-
-        if(l >= r) return 1;
-        else if(s.charAt(l) != s.charAt(r)) return 0;
-        else return recursion(s, l+1, r-1);
-    }
-
-    public static int isPalindrome(String s){
-        return recursion(s, 0, s.length()-1);
-    }
+public class Main {
 
     static int count; // 재귀 호출 횟수를 위한 전역 변수
 
     public static void main(String[] args) throws IOException {
-
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -25,12 +13,22 @@ public class Main{
 
         for(int t=0; t<T; t++) {
             String str = br.readLine();
-
             count = 0;
             bw.write(isPalindrome(str) + " " + count + "\n");
         }
 
         bw.flush();
         bw.close();
+    }
+
+    public static int recursion(String s, int l, int r) {
+        count++; // 재귀 카운트
+        if(l >= r) return 1;
+        else if(s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l+1, r-1);
+    }
+
+    public static int isPalindrome(String s) {
+        return recursion(s, 0, s.length()-1);
     }
 }
