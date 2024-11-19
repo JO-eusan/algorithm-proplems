befe2d2d22cc355b0c76dbc606b208810ad17ba6
San <goodsan0330@gmail.com>
2024-11-19 14:42:01 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 760 ms, Memory: 148692 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -2,27 +2,39 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        final int ALPHA = 'z' - 'a' + 1;
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String str = br.readLine();
         int N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[ALPHA][str.length()];
+
+        for(int i=0; i<ALPHA; i++) {
+            if(i == str.charAt(0) - 'a') {
+                dp[i][0] = 1;
+            }
+
+            for(int j=1; j<str.length(); j++) {
+                dp[i][j] = dp[i][j-1];
+                if(i == str.charAt(j) - 'a') {
+                    dp[i][j]++;
+                }
+            }
+        }
 
         for(int n=0; n<N; n++) {
             String[] tmp = br.readLine().split(" ");
-            char find_char = tmp[0].charAt(0);
+            int find_char = tmp[0].charAt(0) - 'a';
             int i = Integer.parseInt(tmp[1]);
             int j = Integer.parseInt(tmp[2]);
 
-            int sum = 0;
-            for(int k=i; k<=j; k++) {
-                char current = str.charAt(k);
-                if(current == find_char) {
-                    sum++;
-                }
+            int result = dp[find_char][j];
+            if(i > 0) {
+                result -= dp[find_char][i-1];
             }
-            bw.write(String.valueOf(sum) + "\n");
+            bw.write(String.valueOf(result) + "\n");
         }
 
         bw.flush();
