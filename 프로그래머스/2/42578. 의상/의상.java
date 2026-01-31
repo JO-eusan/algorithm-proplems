f2ad186b4e954d17d2998b8809fd4d3e2f685c1e
San <goodsan0330@gmail.com>
2026-01-31 17:22:16 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,23 +2,21 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
+        // 1. clothes를 종류(key) - 개수(value)로 수집한다.
+        Map<String, Integer> categories = new HashMap<>();
         
-        /* count할 자료구조 생성 */
-        HashMap<String, Integer> map = new HashMap<>();
-        
-        for(int i=0; i<clothes.length; i++) {
-            if(!map.containsKey(clothes[i][1]))
-                map.put(clothes[i][1], 1);
-            else
-                map.put(clothes[i][1], map.get(clothes[i][1]) + 1);
+        for (int i = 0; i < clothes.length; i++) {
+            String category = clothes[i][1];
+            categories.put(category, categories.getOrDefault(category, 0) + 1);
         }
         
-        /* 조합 수 계산 */
-        List<Integer> count = new ArrayList<Integer>(map.values());
-        for(int i=0; i<count.size(); i++)
-            answer *= (count.get(i) + 1);
+        // 2. 각 종류를 선택할 경우의 수를 구한다.
+        // 결과: (종류의 개수 + 1)의 곱에 전체가 선택되지 않는 경우 1을 뺀 값
+        int answer = 1;
         
-        return answer-1;
+        for (String category : categories.keySet()) {
+            answer *= (categories.get(category) + 1);
+        }
+        return answer - 1;
     }
 }
