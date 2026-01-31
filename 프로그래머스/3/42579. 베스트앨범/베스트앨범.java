d8dc6d777737531a4f0f6a06f7c15dfd047137ca
San <goodsan0330@gmail.com>
2026-01-31 19:07:20 +0900

    [level 3] Title: 베스트앨범, Time: 1.68 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,43 +2,69 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        List<Integer> answer_tmp = new ArrayList<>();
+        // 1. 장르 별 총 재생 수에 대해 내림차순으로 정렬합니다.
+        int musicNumber = genres.length;
+        Map<String, Integer> counter = new HashMap<>();
         
-        /* 장르 별 총 재생 수 */
-        HashMap<String, Integer> counts = new HashMap<>();
-        
-        for(int i=0; i<genres.length; i++) {
-            if(!counts.containsKey(genres[i]))
-                counts.put(genres[i], plays[i]);
-            else
-                counts.put(genres[i], counts.get(genres[i])+plays[i]);
+        for(int i = 0; i < musicNumber; i++) {
+            String genre = genres[i];
+            int play = plays[i];
+            counter.put(genre, counter.getOrDefault(genre, 0) + play);
         }
         
-        /* 총 재생 수 기준으로 정렬 -> list 활용 */
-        List<String> keySet = new ArrayList<>(counts.keySet());
-        keySet.sort((o1, o2) -> counts.get(o2).compareTo(counts.get(o1)));
+        List<String> sortedGenres = new ArrayList<>(counter.keySet());
+        sortedGenres.sort((o1, o2) -> counter.get(o2).compareTo(counter.get(o1)));
+
         
-        /* 장르 마다 기준에 적합한 앨범 넣기 */
-        for(int i=0; i<keySet.size(); i++) {
-            for(int n=0; n<2; n++) { // 최대 2개의 앨범 선택
-                String max_idx = null;
-                for(int j=0; j<plays.length; j++) {
-                    if(max_idx == null && genres[j].equals(keySet.get(i)) 
-                                                    && !answer_tmp.contains(j))
-                        max_idx = String.valueOf(j);
-                    else if(genres[j].equals(keySet.get(i)) && !answer_tmp.contains(j)
-                            && plays[j] > plays[Integer.parseInt(max_idx)])
-                        max_idx = String.valueOf(j);
-                }
-                if(max_idx != null) answer_tmp.add(Integer.parseInt(max_idx));
+        // 2. 장르 별 노래를 저장합니다.
+        Map<String, List<Music>> musics = new HashMap<>();
+        
+        for(int i = 0; i < musicNumber; i++) {
+            String genre = genres[i];
+            int play = plays[i];
+            if (!musics.keySet().contains(genre)) {
+                musics.put(genre, new ArrayList<>());
             }
+            
+            List<Music> newMusics = musics.get(genre);
+            newMusics.add(new Music(i, play));
+            musics.put(genre, newMusics);
         }
         
-        /* tmp -> answer 배열로 교체 */
-        int[] answer = new int[answer_tmp.size()];
+        // 3. 총 재생 수가 많은 장르부터 노래를 정렬하고 앨범에 최대 2개 노래를 저장합니다.
+        List<Integer> album = new ArrayList<>();
+        for(String genre : sortedGenres) {
+            List<Music> sortedMusic = musics.get(genre);
+            Collections.sort(sortedMusic);
+            
+            for (int i = 0; i < Math.min(2, sortedMusic.size()); i++) {
+    album.add(sortedMusic.get(i).id);
+}
+        }
         
-        for(int i=0; i<answer_tmp.size(); i++)
-            answer[i] = answer_tmp.get(i);
+        // 4. List를 Array로 변환
+        int[] answer = new int[album.size()];
+        for(int i = 0; i < album.size(); i++) {
+            answer[i] = album.get(i);
+        }
         return answer;
+    }
+}
+
+class Music implements Comparable<Music> {
+    int id;
+    int plays;
+    
+    public Music(int id, int plays) {
+        this.id = id;
+        this.plays = plays;
+    }
+    
+    @Override
+    public int compareTo(Music o) {
+        if (this.plays != o.plays) {
+            return o.plays - this.plays; // 내림차순
+        }
+        return this.id - o.id;
     }
 }
