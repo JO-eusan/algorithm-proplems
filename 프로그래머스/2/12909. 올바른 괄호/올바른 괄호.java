39cc4a6bcbe82ff4bde3d2628b09e78c8f1d154a
San <goodsan0330@gmail.com>
2026-02-02 17:19:20 +0900

    [level 2] Title: 올바른 괄호, Time: 22.63 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,26 +2,17 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
         Stack<Character> stack = new Stack<>();
         
-        /* 스택이 비어 있는데 ")"를 push 해야 한다면 false */
-        for(int i=0; i<s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            
-            if(!stack.empty()) {
-                if(c == '(') stack.push('(');
-                else if(c == ')') stack.pop();
-            }
-            else {
-                if(c == '(') stack.push('(');
-                else if(c == ')') answer = false;
+            if (!stack.isEmpty() && c == ')' && stack.peek() == '(') {
+                stack.pop();
+            } else {
+                stack.push(c);
             }
         }
         
-        /* 올바른 괄호라면 stack이 최종적을 비어 있어야 한다. */
-        if(!stack.empty()) answer = false;
-
-        return answer;
+        return stack.isEmpty() ? true : false;
     }
 }
