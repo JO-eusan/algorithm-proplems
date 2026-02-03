5bb6dab237cd5c29aceea6e49d6da5ba455bcb2f
San <goodsan0330@gmail.com>
2026-02-03 12:29:39 +0900

    [level 2] Title: 주식가격, Time: 22.50 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,18 +2,16 @@ class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
         
-        for(int i=0; i<prices.length-1; i++) {
-            int tmp = 0;
-            for(int j=i+1; j<prices.length; j++) {
-                tmp++;
-                if(prices[j] < prices[i]) // 가격 떨어지면
+        for (int i=0; i<prices.length; i++) {
+            int price = prices[i];
+            
+            for (int j=i+1; j<prices.length; j++) {
+                if (prices[j] < price || j == prices.length - 1) {
+                    answer[i] = j - i;
                     break;
+                }
             }
-            answer[i] = tmp;
         }
-        
-        answer[prices.length-1] = 0;
-        
         return answer;
     }
 }
