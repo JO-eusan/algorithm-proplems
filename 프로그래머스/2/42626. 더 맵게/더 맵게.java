b355b1063a0ceb86bf4b4a9526eca9dd09ab8786
San <goodsan0330@gmail.com>
2026-02-04 18:54:01 +0900

    [level 2] Title: 더 맵게, Time: 1493.76 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -4,25 +4,23 @@ class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
         
-        /* min heap을 통해 가장 작은 요소 2개를 뽑고 결과를 put */
-        /* 가장 작은 요소 (tree의 root)가 K 이상까지 반복 */
+        // 1. 스코빌 지수를 우선순위 큐에 넣는다.
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for (int i=0; i<scoville.length; i++) {
+            q.offer(scoville[i]);
+        }
         
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        
-        for(int i=0; i<scoville.length; i++) 
-            minHeap.offer(scoville[i]);
-        
-        while(minHeap.peek() < K) {
-            
-            if(minHeap.size() == 1) {
+        // 2. 스코빌 지수가 가장 낮은 값이 K 이상이 될 때까지 음식을 섞는다.
+        while (q.peek() < K) {
+            // 불가능한 경우 -1을 반환
+            if (q.size() == 1) {
                 answer = -1;
                 break;
             }
+            int n1 = q.poll();
+            int n2 = q.poll();
+            q.offer(n1 + (n2 * 2));
             
-            int min1 = minHeap.poll();
-            int min2 = minHeap.poll();
-            
-            minHeap.offer(min1 + min2*2);
             answer++;
         }
         
