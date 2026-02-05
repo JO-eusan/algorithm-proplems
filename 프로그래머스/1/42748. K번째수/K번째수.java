40870f5539dfd133b982138d0d9444a64634da9a
San <goodsan0330@gmail.com>
2026-02-05 10:21:54 +0900

    [level 1] Title: K번째수, Time: 1.31 ms, Memory: 91.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,21 +4,19 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
-        for(int n=0; n<commands.length; n++) {
+        for (int n=0; n<commands.length; n++) {
+            int i = commands[n][0];
+            int j = commands[n][1];
+            int k = commands[n][2];
             
-            int i=commands[n][0]; int j=commands[n][1]; int k=commands[n][2];
-            
-            /* 배열 자르기 */
-            int[] tmp = new int[j-i+1];
             int idx = 0;
-            for(int t=i-1; t<=j-1; t++) {
-                tmp[idx++] = array[t];
+            int[] tmp = new int[j - i + 1];
+            for(int m=i-1; m<=j-1; m++) {
+                tmp[idx++] = array[m];
             }
             
-            /* 정렬 */
             Arrays.sort(tmp);
-            
-            answer[n] = tmp[k-1];
+            answer[n] = tmp[k - 1];
         }
         
         return answer;
