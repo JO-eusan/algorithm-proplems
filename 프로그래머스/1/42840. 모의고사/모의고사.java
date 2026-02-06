26875cabcfbb998ec4dfdc1b9c4f35c4d12bf81b
San <goodsan0330@gmail.com>
2026-02-06 11:57:44 +0900

    [level 1] Title: 모의고사, Time: 3.45 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,77 +2,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] answer = {};
+        int[] p1 = {1,2,3,4,5};
+        int[] p2 = {2,1,2,3,2,4,2,5};
+        int[] p3 = {3,3,1,1,2,2,4,4,5,5};
         
-        /* 수포자 1 답안지 */
-        int[] answer_1 = new int[answers.length];
-        int cnt = 0;
-        for(int i=0; i<answers.length; i++) {
-            answer_1[i] = (cnt%5) + 1;
-            cnt++;
+        int[] score = new int[3];
+
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == p1[i % p1.length]) score[0]++;
+            if (answers[i] == p2[i % p2.length]) score[1]++;
+            if (answers[i] == p3[i % p3.length]) score[2]++;
         }
         
-        /* 수포자 2 답안지 */
-        int[] answer_2 = new int[answers.length] ;
-        cnt = 0;
-        for(int i=0; i<answers.length; i++) {
-            if(i%2 == 0)
-                answer_2[i] = 2;
-            else {
-                int tmp = (cnt%5) + 1;
-                if(tmp == 2) {
-                    cnt++;
-                    answer_2[i] = (cnt%5) + 1;
-                }
-                else {
-                    answer_2[i] = tmp;
-                }
-                cnt++;
+        int maxIdx = 0;
+        if(score[1] > score[maxIdx]) {
+            maxIdx = 1;
+        }
+        if(score[2] > score[maxIdx]) {
+            maxIdx = 2;
+        }
+        
+        List<Integer> result = new ArrayList<>();
+        for(int i=0; i<3; i++) {
+            if(score[i] == score[maxIdx]) {
+                result.add(i + 1);
             }
         }
         
-        /* 수포자 3 답안지 */
-        int[] answer_3 = new int[answers.length] ;
-        cnt = 0;
-        int[] sequence = {3, 1, 2, 4, 5}; 
-        for(int i=0; i<answers.length; i++) {
-            answer_3[i] = sequence[cnt%5];
-            
-            if(i%2 == 1) cnt++;
-        } 
-        
-        for(int i=0; i<answers.length; i++) {
-            System.out.print(answer_3[i] + " ");
-        }
-        
-        /* 정답 개수 세기 */
-        int cnt_1 = 0, cnt_2 = 0, cnt_3 = 0;
-        for(int i=0; i<answers.length; i++) {
-            if(answer_1[i] == answers[i])
-                cnt_1++;
-            if(answer_2[i] == answers[i])
-                cnt_2++;
-            if(answer_3[i] == answers[i])
-                cnt_3++;
-        }
-        
-        /* 최대값 찾기 */
-        int max = cnt_1;
-        if(cnt_2 > max) max = cnt_2;
-        if(cnt_3 > max) max = cnt_3;
-        
-        List<Integer> list = new ArrayList<>();
-        if(max == cnt_1) list.add(1);
-        if(max == cnt_2) list.add(2);
-        if(max == cnt_3) list.add(3);
-        
-        Collections.sort(list);
-        
-        answer = new int[list.size()];
-        for(int i=0; i<list.size(); i++) {
-            answer[i] = list.get(i);
-        }
-        
-        return answer;
+        Collections.sort(result);
+        return result.stream().mapToInt(i -> i).toArray();
     }
 }
