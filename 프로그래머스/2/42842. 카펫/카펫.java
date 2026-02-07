32f6e655784f7a163b59305b1004ddd0831a9962
San <goodsan0330@gmail.com>
2026-02-07 15:25:46 +0900

    [level 2] Title: 카펫, Time: 15.79 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,18 +1,29 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
+        // 1. yellow 개수로 만들 수 있는 경우의 수를 구한다.
+        Set<Integer> firstPair = new HashSet<>();
         
-        // yellow -> 사각형 모양 맞추기
-        for(int i=1; i<=Math.sqrt(yellow); i++) { // 세로
-            int depth = i;
-            int width = yellow / i;
-            if(brown == ((depth + 2)*2 + width*2)) {
-                answer[0] = width + 2;
-                answer[1] = depth + 2;
+        for(int i=1; i<=yellow; i++) {
+            if(yellow % i == 0 && !firstPair.contains(yellow / i)) {
+                firstPair.add(i);
             }
-                
         }
         
+        // 2. 해당 원소의 값으로 brown의 개수를 일치하는 값을 찾는다.
+        int[] answer = new int[2];
+        
+        for(int pair : firstPair) {
+            int maxLength = Math.max(pair, yellow / pair);
+            int minLength = Math.min(pair, yellow / pair);
+            
+            if((2 * (maxLength + minLength) + 4) == brown) {
+                answer[0] = maxLength + 2;
+                answer[1] = minLength + 2;
+                break;
+            }
+        }
         return answer;
     }
 }
