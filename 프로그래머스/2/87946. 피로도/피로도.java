41ec152faf744f4ed52d66c05c64b8e0f9810d95
San <goodsan0330@gmail.com>
2026-02-07 16:13:43 +0900

    [level 2] Title: 피로도, Time: 21.19 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -2,52 +2,24 @@ import java.util.*;
 
 class Solution {
     
-    private static int n;
-    private static int[] arr; // 선택 원소
-    private static boolean[] visited; // 방문 처리
-    private static List<Integer> list = new ArrayList<>();
+    int maxValue;
     
-    public int solution(int k, int[][] dungeons) {
-        int answer = 0;
-        
-        n = dungeons.length;
-        arr = new int[n];
-        visited = new boolean[n];
-        
-        permutation(0, k, dungeons);
-        
-        for(int i=0; i<list.size(); i++) {
-            if(answer < list.get(i))
-                answer = list.get(i);
+    public void recursive(int k, int[][] dungeons, List<Integer> picked) {
+        if(picked.size() > maxValue) {
+            maxValue = picked.size();
         }
         
-        return answer;
+        for(int i=0; i<dungeons.length; i++) {
+            if(!picked.contains(i) && k >= dungeons[i][0]) {
+                List<Integer> newPicked = new ArrayList<>(picked);
+                newPicked.add(i);
+                recursive(k - dungeons[i][1], dungeons, newPicked);
+            }
+        }
     }
     
-    private static void permutation(int cnt, int k, int[][] dungeons) {
-        if(cnt == n) {
-            int count = 0;
-            int k_tmp = k;
-            for(int i=0; i<arr.length; i++) {
-                
-                if(k_tmp >= dungeons[arr[i]][0]) {
-                    k_tmp -= dungeons[arr[i]][1];
-                    count++;
-                }
-                else break;
-            }
-            
-            list.add(count);
-            return;
-        }
-        
-        for(int i=0; i<n; i++) {
-            if(!visited[i]) {
-                visited[i] = true;
-                arr[cnt] = i;
-                permutation(cnt + 1, k, dungeons);
-                visited[i] = false;
-            }
-        }
+    public int solution(int k, int[][] dungeons) {
+        recursive(k, dungeons, new ArrayList<>());
+        return maxValue;
     }
 }
