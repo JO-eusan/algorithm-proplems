3914a005e274df4bb50a4e0382a9c2c8a9690c16
San <goodsan0330@gmail.com>
2026-02-08 21:31:28 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 16.77 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,53 +1,56 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int n, int[][] wires) {
-        int answer = Integer.MAX_VALUE;
-        
-        // 인접 행렬 생성
-        int[][] graph = new int[n+1][n+1];
     
-        for(int[] wire : wires) {
-            graph[wire[0]][wire[1]] = 1;
-            graph[wire[1]][wire[0]] = 1;
-        }
-        
-        // 하나의 전선을 끊으면서 탐색
-        for(int[] wire : wires) {
-            int node1 = wire[0];
-            int node2 = wire[1];
-            
-            /* 연결 끊기 */
-            graph[node1][node2] = 0;
-            graph[node2][node1] = 0;
-            
-            int subtreeSize = dfs(graph, new boolean[n+1], node1, n);
-            int diff = Math.abs((n-subtreeSize) - subtreeSize);
-            answer = Math.min(answer, diff); // 최소값 선택
-            
-            /* 연결 복구 */
-            graph[node1][node2] = 1;
-            graph[node2][node1] = 1;
-        }
-        
-        
-        return answer;
-    }
+    boolean[][] lines;
     
-    public int dfs(int[][] graph, boolean[] visited, int node, int n) {
-        visited[node] = true; // 시작 노드
+    public int bfs(int n, int start) {
+        boolean[] visited = new boolean[n + 1];
+        Queue<Integer> q = new LinkedList<>();
+        
+        q.offer(start);
+        visited[start] = true;
+        
         int count = 1;
         
-        /* search */
-        for(int i=1; i<=n; i++) { 
-            // 시작 노드와 연결 되어 있고 방문하지 않은 곳이라면 count 
-            if(graph[node][i] == 1 && !visited[i]) {
-                count += dfs(graph, visited, i, n); 
+        while(!q.isEmpty()) {
+            int num = q.poll();
+            
+            for(int i=1; i<=n; i++) {
+                if(lines[num][i] && !visited[i]) {
+                    visited[i] = true;
+                    q.offer(i);
+                    count++;
+                }
             }
         }
+        return Math.abs(count - (n - count));
+    }
+    
+    public int solution(int n, int[][] wires) {
+        int answer = n;
+        lines = new boolean[n + 1][n + 1];
         
-        return count;
+        // 1. 연결된 전선을 인접 행렬에 저장합니다.
+        for(int i=0; i<wires.length; i++) {
+            lines[wires[i][0]][wires[i][1]] = true;
+            lines[wires[i][1]][wires[i][0]] = true;
+        }
         
+        // 2. 선을 하나씩 끊어보면서 순회합니다.
+        for(int i=0; i<wires.length; i++) {
+            int x = wires[i][0];
+            int y = wires[i][1];
+            
+            lines[x][y] = false;
+            lines[y][x] = false;
+            
+            answer = Math.min(answer, bfs(n, x));
+            
+            lines[x][y] = true;
+            lines[y][x] = true;
+        }
         
+        return answer;
     }
 }
