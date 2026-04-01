104e9de422853f6dd5a0cf2546df8c6f63bf47b7
San <goodsan0330@gmail.com>
2026-04-01 20:24:02 +0900

    [Gold V] Title: 회의실 배정, Time: 404 ms, Memory: 41824 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -1,43 +1,44 @@
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        long[][] times = new long[N][2]; // [start][end]
-
-        for(int i=0; i<N; i++) {
-            String[] tokens = br.readLine().split(" ");
-            times[i][0] = Long.parseLong(tokens[0]);
-            times[i][1] = Long.parseLong(tokens[1]);
-        }
-
-        Arrays.sort(times, new Comparator<long[]>() {
+                
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
             @Override
-            public int compare(long[] o1, long[] o2) {
-                if(o1[1] == o2[1]) {
-                    return (int)(o1[0] - o2[0]);
+            public int compare(int[] a, int[] b) {
+                if (a[1] == b[1]) {
+                    return a[0] - b[0];
                 }
-                return (int)(o1[1] - o2[1]);
+                return a[1] - b[1];
             }
         });
+        
+        StringTokenizer st;
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
 
-        int counter = 1; // 첫번째 작업 추가
-        long nextStart = times[0][1];
-
-        for(int i=1; i<N; i++) {
-            if(times[i][0] >= nextStart) {
-                counter++;
-                nextStart = times[i][1];
-            }
+            pq.offer(new int[] {start, end});
         }
 
-        bw.write(String.valueOf(counter));
+        int answer = 0;
+        int lastTime = 0;
+        while (!pq.isEmpty()) {
+            int[] arr = pq.poll();
 
-        bw.flush();
-        bw.close();
+            if (lastTime <= arr[0]) {
+                answer++;
+                lastTime = arr[1];
+            }
+        }
+        
+        System.out.println(answer);
     }
 }
