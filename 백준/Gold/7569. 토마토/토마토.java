9cfb1c282589b0b8dc7ce911e8d8924819b942e0
San <goodsan0330@gmail.com>
2026-04-23 19:28:19 +0900

    [Gold V] Title: 토마토, Time: 588 ms, Memory: 119792 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -8,8 +8,7 @@ public class Main {
     private static final int[] dy = {0, 0, 1, -1, 0, 0};
     private static final int[] dz = {0, 0, 0, 0, 1, -1};
 
-    private static int[][][] board;
-    private static boolean[][][] visited;
+    private static int emptySpace = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,9 +18,7 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int H = Integer.parseInt(st.nextToken());
 
-        board = new int[N][M][H];
-        visited = new boolean[N][M][H];
-
+        int[][][] board = new int[N][M][H];
         List<int[]> tomatoes = new ArrayList<>();
 
         for(int h=0; h<H; h++) {
@@ -31,66 +28,53 @@ public class Main {
                 for(int m=0; m<M; m++) {
                     int value = Integer.parseInt(st.nextToken());
                     board[n][m][h] = value;
-                    visited[n][m][h] = (value != 0);
 
                     if (value == 1) {
                         tomatoes.add(new int[] {n, m, h});
+                    }
+                    if (value == 0) {
+                        emptySpace++;
                     }
                 }
             }
         }
         
-        int counter = bfs(tomatoes, N, M, H);
+        int counter = bfs(tomatoes, board, N, M, H);
 
-        if (isCompleteTomatoes(N, M, H)) {
-            System.out.println(counter);
-        } else {
+        if (emptySpace > 0) {
             System.out.println(-1);
+        } else {
+            System.out.println(counter);
         }
     }
 
-    private static int bfs(List<int[]> starts, int N, int M, int H) {
+    private static int bfs(List<int[]> starts, int[][][] board, int N, int M, int H) {
         Queue<int[]> q = new LinkedList<>(starts);
 
-        int counter = -1;
+        int counter = 0;
 
         while (!q.isEmpty()) {
-            int size = q.size();
+            int[] current = q.poll();            
             
-            for(int i=0; i<size; i++) {
-                int[] current = q.poll();
+            for(int j=0; j<6; j++) {
+                int newN = current[0] + dx[j];
+                int newM = current[1] + dy[j];
+                int newH = current[2] + dz[j];
 
-                for(int j=0; j<6; j++) {
-                    int newN = current[0] + dx[j];
-                    int newM = current[1] + dy[j];
-                    int newH = current[2] + dz[j];
+                if (newN < 0 || newN >= N || newM < 0 || newM >= M || newH < 0 || newH >= H) {
+                    continue;
+                }
 
-                    if (newN < 0 || newN >= N || newM < 0 || newM >= M || newH < 0 || newH >= H) {
-                        continue;
-                    }
-
-                    if (!visited[newN][newM][newH]) {
-                        q.offer(new int[] {newN, newM, newH});
-                        visited[newN][newM][newH] = true;
-                    }
+                if (board[newN][newM][newH] == 0) {
+                    q.offer(new int[] {newN, newM, newH});
+                    board[newN][newM][newH] = board[current[0]][current[1]][current[2]] + 1;
+                    
+                    emptySpace--;
+                    counter = Math.max(counter, board[current[0]][current[1]][current[2]]);
                 }
             }
-            counter++;
         }
 
         return counter;
-    }
-
-    private static boolean isCompleteTomatoes(int N, int M, int H) {
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<M; j++) {
-                for(int k=0; k<H; k++) {
-                    if (!visited[i][j][k]) {
-                        return false;
-                    }
-                }
-            }
-        }
-        return true;
     }
 }
