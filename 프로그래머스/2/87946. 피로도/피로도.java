1fe8cda5133d3c7220d229f30eb8e865049f992f
San <goodsan0330@gmail.com>
2026-06-29 11:01:36 +0900

    [level 2] Title: 피로도, Time: 2.54 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,25 +1,28 @@
-import java.util.*;
-
 class Solution {
     
-    int maxValue;
+    int max = 0;
     
-    public void recursive(int k, int[][] dungeons, List<Integer> picked) {
-        if(picked.size() > maxValue) {
-            maxValue = picked.size();
+    public int solution(int k, int[][] dungeons) {
+        dfs(k, dungeons, 0, 0);
+        return max;
+    }
+    
+    private void dfs(int hp, int[][] dungeons, int mask, int cnt) {
+        if(mask == (1<<dungeons.length) - 1) {
+            max = Math.max(max, cnt);
+            return;
         }
         
         for(int i=0; i<dungeons.length; i++) {
-            if(!picked.contains(i) && k >= dungeons[i][0]) {
-                List<Integer> newPicked = new ArrayList<>(picked);
-                newPicked.add(i);
-                recursive(k - dungeons[i][1], dungeons, newPicked);
+            if((mask & (1<<i)) == 0) {
+                // 피로도 조건이 맞다면 던전 탐험
+                if(hp >= dungeons[i][0]) {
+                    dfs(hp-dungeons[i][1], dungeons, mask | (1<<i), cnt+1);
+                } else {
+                    // 아니라면 방문 체크만
+                    dfs(hp, dungeons, mask | (1<<i), cnt);
+                }
             }
         }
-    }
-    
-    public int solution(int k, int[][] dungeons) {
-        recursive(k, dungeons, new ArrayList<>());
-        return maxValue;
     }
 }
