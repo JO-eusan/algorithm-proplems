dba7fc5fabf755bd66b173c1b98a9a955dda0aa8
San <goodsan0330@gmail.com>
2026-07-18 18:09:09 +0900

    [level 3] Title: 합승 택시 요금, Time: 18.25 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,68 +1,62 @@
 import java.util.*;
 
 class Solution {
-    
     public int solution(int n, int s, int a, int b, int[][] fares) {
         List<int[]>[] graph = new ArrayList[n+1];
-        for (int i=1; i<=n; i++) {
+        for(int i=1; i<=n; i++) {
             graph[i] = new ArrayList<>();
         }
         
-        for (int i=0; i<fares.length; i++) {
-            int u = fares[i][0];
-            int v = fares[i][1];
-            int w = fares[i][2];
+        for(int[] f : fares) {
+            int u = f[0];
+            int v = f[1];
+            int c = f[2];
             
-            graph[u].add(new int[] {v, w});
-            graph[v].add(new int[] {u, w});
+            graph[u].add(new int[] {v, c});
+            graph[v].add(new int[] {u, c});
         }
         
-        int[] distFromS = dijkstra(graph, s);
-        int[] distFromA = dijkstra(graph, a);
-        int[] distFromB = dijkstra(graph, b);
+        int[] dist_s = dikjstra(graph, s); // start: s
+        int[] dist_a = dikjstra(graph, a); // start: a
+        int[] dist_b = dikjstra(graph, b); // start: b
         
-        int separation = distFromS[a] + distFromS[b]; // s - A and s - B
+        int separation = dist_s[a] + dist_s[b];
         int together = Integer.MAX_VALUE;
         
-        for (int i=1; i<=n; i++) {
-            int distance = distFromS[i] + distFromA[i] + distFromB[i];
-            
-            if (distance < together) {
-                together = distance;
-            }
+        for(int m=1; m<=n; m++) {
+            together = Math.min(together, dist_s[m] + dist_a[m] + dist_b[m]);
         }
         
-        return Math.min(together, separation);
+        return Math.min(separation, together);
     }
     
-    private int[] dijkstra(List<int[]>[] graph, int start) {
-        int[] dist = new int[graph.length + 1];
+    private int[] dikjstra(List<int[]>[] graph, int start) {
+        int[] dist = new int[graph.length];
         Arrays.fill(dist, Integer.MAX_VALUE);
         
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
         pq.offer(new int[] {start, 0});
         dist[start] = 0;
         
-        while (!pq.isEmpty()) {
+        while(!pq.isEmpty()) {
             int[] now = pq.poll();
             int node = now[0];
             int cost = now[1];
             
-            if (dist[node] < cost) {
+            if(dist[node] < cost) {
                 continue;
             }
             
-            for (int[] next : graph[node]) {
-                int nextNode = next[0];
-                int nextCost = next[1];
+            for(int[] next : graph[node]) {
+                int nc = dist[node] + next[1];
                 
-                int newCost = cost + nextCost;
-                if (newCost < dist[nextNode]) {
-                    dist[nextNode] = newCost;
-                    pq.offer(new int[] {nextNode, newCost});
+                if(nc < dist[next[0]]) {
+                    dist[next[0]] = nc;
+                    pq.offer(new int[] {next[0], nc});
                 }
             }
         }
+        
         return dist;
     }
 }
