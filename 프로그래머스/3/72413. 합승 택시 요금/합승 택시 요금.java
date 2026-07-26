d4a6bf123cb82a6193e5f68da9a1b14df342a286
San <goodsan0330@gmail.com>
2026-07-26 13:10:28 +0900

    [level 3] Title: 합승 택시 요금, Time: 18.91 ms, Memory: 68.7 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,37 +1,38 @@
 import java.util.*;
 
 class Solution {
+    
+    private List<int[]>[] graph;
+    
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        List<int[]>[] graph = new ArrayList[n+1];
+        graph = new ArrayList[n+1];
+        
         for(int i=1; i<=n; i++) {
             graph[i] = new ArrayList<>();
         }
         
         for(int[] f : fares) {
-            int u = f[0];
-            int v = f[1];
-            int c = f[2];
-            
-            graph[u].add(new int[] {v, c});
-            graph[v].add(new int[] {u, c});
+            graph[f[0]].add(new int[] {f[1], f[2]});
+            graph[f[1]].add(new int[] {f[0], f[2]});
         }
         
-        int[] dist_s = dikjstra(graph, s); // start: s
-        int[] dist_a = dikjstra(graph, a); // start: a
-        int[] dist_b = dikjstra(graph, b); // start: b
+        int[] distS = dijkstra(s, n);
+        int[] distA = dijkstra(a, n);
+        int[] distB = dijkstra(b, n);
         
-        int separation = dist_s[a] + dist_s[b];
-        int together = Integer.MAX_VALUE;
+        // 'S -> X' + 'X -> A' + 'X -> B'
+        // 'S -> A' + 'S -> B'
+        int separation = Integer.MAX_VALUE;
         
-        for(int m=1; m<=n; m++) {
-            together = Math.min(together, dist_s[m] + dist_a[m] + dist_b[m]);
+        for(int i=1; i<=n; i++) {
+            int total = distS[i] + distA[i] + distB[i];
+            separation = Math.min(separation, total);
         }
-        
-        return Math.min(separation, together);
+        return Math.min(separation, distS[a] + distS[b]);
     }
     
-    private int[] dikjstra(List<int[]>[] graph, int start) {
-        int[] dist = new int[graph.length];
+    private int[] dijkstra(int start, int n) {
+        int[] dist = new int[n+1];
         Arrays.fill(dist, Integer.MAX_VALUE);
         
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
@@ -43,20 +44,19 @@ class Solution {
             int node = now[0];
             int cost = now[1];
             
-            if(dist[node] < cost) {
-                continue;
-            }
+            if(dist[node] < cost) continue;
             
             for(int[] next : graph[node]) {
-                int nc = dist[node] + next[1];
+                int nextNode = next[0];
+                int nextCost = next[1];
                 
-                if(nc < dist[next[0]]) {
-                    dist[next[0]] = nc;
-                    pq.offer(new int[] {next[0], nc});
+                int newCost = dist[node] + nextCost;
+                if(newCost < dist[nextNode]) {
+                    pq.offer(new int[] {nextNode, newCost});
+                    dist[nextNode] = newCost;
                 }
             }
         }
-        
         return dist;
     }
 }
