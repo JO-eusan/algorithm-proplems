53e5eca41d4610297fcc4ae5547926a8eb34c1b3
San <goodsan0330@gmail.com>
2026-07-28 22:10:25 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.29 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -1,44 +1,43 @@
 class Solution {
     /*
-     * 붕대 감기 = t초 동안 붕대를 감으면 1초마다 x만큼 체력 회복
-     * t초 연속으로 붕대 감는거 성공하면 +y 만큼 추가 체력 회복
-     * 현재 체력이 최대 체력보다 커질 수는 없음
-     * 몬스터 공격 당하면 체력 회복 X -> 연속 성공 시간 0
-     * 몬스터 공격 당하면 정해진 피해량만큰 현재 체력 감소
-     * 현재 체력이 0이하가 되면 캐릭터는 죽어서 체력 회복 중단 후 -1 return
-     *
-     * bandage[0] = 붕대감기 기술의 시전시간
-     * bandage[1] = 초당 회복량
-     * bandage[2] = 추가 회복량
-     * health = 최대 체력
+     * 붕대 감기
+     * - t초 동안 감으면서 1초마다 x만큼 체력 회복 (+ t*x)
+     * - t초 연속 붕대 감기 성공 -> + y만큼 추가 회복
+     * - 최대 체력보다 커지는 것은 불가능
+     * - 몬스터 공격 -> 연속 성공 시간 0초, -피해량
+     * - 현재 체력 0 이하면 죽고 체력 회복 불가능
     */
-    
     public int solution(int[] bandage, int health, int[][] attacks) {
-        int cur_health = health;
-        int sequence = 0;
+        int t = bandage[0];
+        int x = bandage[1];
+        int y = bandage[2];
         
-        int attack_idx = 0;
+        int nowHealth = health;
+        int idx = 0;
+        int dur = 0;
         
-        for(int time=1; time<=attacks[attacks.length-1][0]; time++) {
-            if(attack_idx < attacks.length && time == attacks[attack_idx][0]) {
-                cur_health -= attacks[attack_idx][1]; // 공격
-                attack_idx++;
-                sequence = 0;
+        for(int now=1; now<=attacks[attacks.length-1][0]; now++) {            
+            if(now == attacks[idx][0]) {
+                nowHealth -= attacks[idx][1];
+                idx++;
+                dur = 0;
                 
-                if(cur_health <= 0) {
-                    return -1;
-                }
-            } else {
-                cur_health = Math.min(cur_health + bandage[1], health); // 회복
-                sequence++;
-                
-                if(sequence == bandage[0]) {
-                    cur_health = Math.min(cur_health + bandage[2], health); // 추가 회복
-                    sequence = 0;
-                }
+                if(nowHealth <= 0) return -1;
+                continue;
             }
+            
+            nowHealth += x;
+            dur++;
+            
+            if(dur == t) {
+                nowHealth += y;
+                dur = 0;
+            }
+            nowHealth = Math.min(nowHealth, health);
+            
+            if(nowHealth <= 0) return -1;
         }
         
-        return cur_health <= 0 ? -1 : cur_health;
+        return nowHealth;
     }
 }
