12e22cf7ad828ec99ded8610de34e3c9355a2f82
San <goodsan0330@gmail.com>
2026-07-29 23:57:05 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 203.97 ms, Memory: 369 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -1,67 +1,77 @@
 import java.util.*;
 
 class Solution {
+    /*
+     * 포인트 1~n : (r, c)로 표현
+     * 로봇마다 운송 경로 존재 : m개의 포인트로 구성해 순서대로 방문
+     * 사용 로봇은 x대이고 모든 로봇은 0초에 동시에 출발하고 1초마다 1칸씩 이동
+     * 다음 포인트로 이동하는 경우 항상 최단 경로로 이동 (r좌표 이동 우선)
+    */
     
     public int solution(int[][] points, int[][] routes) {
-        List<List<int[]>> robots = new ArrayList<>();
+        int x = routes.length; 
         
-        int maxTime = 0;
+        List<int[]>[] paths = new ArrayList[x];
         
-        for(int[] route : routes) {
-            // 로봇별 이동 경로 생성
-            List<int[]> path = new ArrayList<>();
-            
-            // 첫 위치
-            int[] cur = points[route[0]-1];
-            path.add(new int[] {cur[0], cur[1]});
-            
-            for(int i=1; i<route.length; i++) {
-		            // 다음 목적지
-                int[] next = points[route[i]-1];
-                
-                // r축 방향으로 먼저 이동
-                while(cur[0] != next[0]) {
-                    if(cur[0] < next[0]) { // (+)
-                        cur = new int[] {cur[0]+1, cur[1]};
-                    } else { // (-)
-                        cur = new int[] {cur[0]-1, cur[1]};
-                    }
-                    path.add(new int[] {cur[0], cur[1]});
-                }
-                
-                // c축 방향으로 이동해 도착지 가기
-                while(cur[1] != next[1]) {
-                    if(cur[1] < next[1]) { // (+)
-                        cur = new int[] {cur[0], cur[1]+1};
-                    } else { // (-)
-                        cur = new int[] {cur[0], cur[1]-1};
-                    }
-                    path.add(new int[] {cur[0], cur[1]});
-                }
-            }
-            
-            maxTime = Math.max(maxTime, path.size());
-            robots.add(path);
+        for(int i=0; i<x; i++) {
+            paths[i] = new ArrayList<>();
         }
         
-        // 시간마다 로봇의 개수가 2이상인지 확인해 충돌 검사
-        int answer = 0;
-        
-        for(int t=0;t<maxTime;t++){
-            int[][] count = new int[101][101];
-            for(List<int[]> robot : robots){
-
-                if(t < robot.size()){
-                    int[] pos = robot.get(t);
-                    count[pos[0]][pos[1]]++;
+        for(int i=0; i<routes.length; i++) {
+            int[] route = routes[i];
+            int[] start = points[route[0] - 1];
+            paths[i].add(new int[]{start[0], start[1]});
+            
+            for(int j=1; j<route.length; j++) {
+                int[] prev = points[route[j-1] - 1];
+                int[] now = points[route[j] - 1];
+                
+                if(prev[0] - now[0] > 0) {
+                    for(int r=prev[0]-1; r>=now[0]; r--) {
+                        paths[i].add(new int[] {r, prev[1]});
+                    }
+                } else if(prev[0] - now[0] < 0) {
+                    for(int r=prev[0]+1; r<=now[0]; r++) {
+                        paths[i].add(new int[] {r, prev[1]});
+                    }
+                }
+                
+                if(prev[1] - now[1] > 0) {
+                    for(int c=prev[1]-1; c>=now[1]; c--) {
+                        paths[i].add(new int[] {now[0], c});
+                    }
+                } else if(prev[1] - now[1] < 0) {
+                    for(int c=prev[1]+1; c<=now[1]; c++) {
+                        paths[i].add(new int[] {now[0], c});
+                    }
                 }
             }
-
-            for(int i=1;i<=100;i++){
-                for(int j=1;j<=100;j++){
-                    if(count[i][j] >= 2)
-                        answer++;
+        }
+        
+        int time = paths[0].size();
+        for(int i=1; i<x; i++) {
+            time = Math.max(paths[i].size(), time);
+        }
+        
+        int answer = 0;
+        
+        for(int t=0; t<time; t++) {
+            List<int[]> pairs = new ArrayList<>();
+            
+            for(int i=0; i<x; i++) {
+                List<int[]> path = paths[i];
+                if(path.size() > t) {
+                    pairs.add(path.get(t));
                 }
+            }
+            
+            int[][] visited = new int[101][101];
+            for(int[] p : pairs) {
+                if(visited[p[0]][p[1]] == 1) {
+                    answer++;
+                    visited[p[0]][p[1]]++;
+                }
+                visited[p[0]][p[1]]++;
             }
         }
         
