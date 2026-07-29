202ee441088aa3ec29918a56a01a7eb3f2965983
San <goodsan0330@gmail.com>
2026-07-29 20:37:56 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 33.55 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,55 +1,60 @@
 import java.util.*;
 
 class Solution {
+    
     /*
-    * diff = 현재 퍼즐의 난이도
-    * time_cur = 현재 퍼즐의 소요시간
-    * time_prev = 이전 퍼즐의 소요시간
-    * level = 숙련도
-    * limit = 전체 제한 시간
+     * n개의 퍼즐은 난이도, 소요시간 존재
+     * diff : 현재 퍼즐의 난이도
+     * time_cur : 현재 퍼즐의 소요 시간
+     * time_prev : 이전 퍼즐의 소요 시간
+     * level : 숙련도
+     * limit : 전체 제한 시간
     */
+    
     public int solution(int[] diffs, int[] times, long limit) {
-        int max_diff = 1;
-        for(int i=1; i<diffs.length; i++) {
-            if(diffs[i] > max_diff) {
-                max_diff = diffs[i];
-            }
+        // time_prev 메모이제이션
+        int[] time_prev = new int[times.length];
+        
+        for(int i=1; i<times.length; i++) {
+            time_prev[i] = times[i-1];
         }
         
+        // 이분탐색으로 target(level) 최솟값 계산
+        int answer = Integer.MAX_VALUE;
         int left = 1;
-        int right = max_diff;
-        
-        int level = max_diff;
+        int right = 100000;
         
         while(left <= right) {
             int mid = (left + right) / 2;
             
-            if(isAvailable(diffs, times, mid, limit)) {
-                level = mid;
+            if(isAvailable(diffs, times, time_prev, limit, mid)) {
                 right = mid - 1;
+                answer = Math.min(answer, mid);
             } else {
                 left = mid + 1;
             }
         }
-    
-        return level;
+        
+        // 제한 시간 내 퍼즐을 모두 해결하기 위한 숙련도의 최솟값
+        return answer;
     }
     
-    private boolean isAvailable(int[] diffs, int[] times, int level, long limit) {
-        long total_time = times[0];
+    private boolean isAvailable(int[] diffs, int[] times, int[] time_prev, long limit, int level) {
+        long total = 0;
         
-        for(int i=1; i<diffs.length; i++) {
+        for(int i=0; i<diffs.length; i++) {
             int diff = diffs[i];
-            int time_cur = times[i];
-            int time_prev = times[i-1];
+            int t_cur = times[i];
+            int t_prev = time_prev[i];
             
             if(diff <= level) {
-                total_time += time_cur;
+                total += t_cur;
             } else {
-                total_time += (diff-level) * (time_cur+time_prev) + time_cur;
+                int repeat = diff - level;
+                total += (long) ((t_prev + t_cur) * repeat + t_cur);
             }
         }
         
-        return total_time <= limit;
+        return total <= limit;
     }
 }
