a34816f4c654039b1c68788dee5d1935abbead0a
San <goodsan0330@gmail.com>
2026-07-29 22:36:00 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 44.80 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -2,81 +2,78 @@ import java.util.*;
 
 class Solution {
     
-    private int[] dx = {1, -1, 0, 0};
-    private int[] dy = {0, 0, 1, -1};
+    /*
+     * BFS로 영역별 덩어리 크기 측정
+     * 행(m) 길이만큼 순회하면서 Set으로 덩어리 번호 수집 후 크기 최대
+    */
+    
+    private final int[] dx = {1, -1, 0, 0};
+    private final int[] dy = {0, 0, 1, -1};
     
     public int solution(int[][] land) {
         int n = land.length;
         int m = land[0].length;
         
-        boolean[][] visited = new boolean[n][m];
-        
-        HashMap<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> oil_area = new HashMap<>();
         int[][] area = new int[n][m];
-        
-        int idx = 1;
+        int area_idx = 1;
         
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
-                if(!visited[i][j] && land[i][j] == 1) {
-                    int total = bfs(idx, i, j, area, visited, land);
-                    map.put(idx, total);
-                    idx++;
+                if(land[i][j] == 1 && area[i][j] == 0) {
+                    int value = bfs(i, j, land, area, area_idx);
+                    oil_area.put(area_idx, value);
+                    area_idx++;
                 }
             }
         }
         
         int answer = 0;
         
-        for(int i=0; i<m; i++) {
-            HashSet<Integer> set = new HashSet<>();
-            int total = 0;
+        for(int j=0; j<m; j++) {
+            Set<Integer> a = new HashSet<>();
+            int sum = 0;
             
-            for(int j=0; j<n; j++) {
-                int now = area[j][i];
-                if(now != 0 && !set.contains(now)) {
-                    set.add(now);
-                    total += map.get(now);
+            for(int i=0; i<n; i++) {
+                if(area[i][j] != 0 && !a.contains(area[i][j])) {
+                    sum += oil_area.get(area[i][j]);
+                    a.add(area[i][j]);
                 }
             }
             
-            if (total > answer) {
-                answer = total;
-            }
+            answer = Math.max(sum, answer);
         }
         
         return answer;
     }
     
-    private int bfs(int idx, int x, int y, int[][] area, boolean[][] visited, int[][] land) {
+    private int bfs(int startX, int startY, int[][] land, int[][] area, int area_idx) {
         int n = land.length;
         int m = land[0].length;
         
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {x, y});
-        area[x][y] = idx;
-        visited[x][y] = true;
-        
-        int size = 1;
+        q.offer(new int[] {startX, startY});
+        area[startX][startY] = area_idx;
+        int width = 1;
         
         while(!q.isEmpty()) {
             int[] now = q.poll();
+            int x = now[0];
+            int y = now[1];
             
             for(int i=0; i<4; i++) {
-                int nx = now[0] + dx[i];
-                int ny = now[1] + dy[i];
+                int nx = x + dx[i];
+                int ny = y + dy[i];
                 
-                if(nx < 0 || nx >= n || ny < 0 || ny >= m) {
-                    continue;
-                }
-                if(!visited[nx][ny] && land[nx][ny] == 1) {
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                
+                if(land[nx][ny] == 1 && area[nx][ny] == 0) {
                     q.offer(new int[] {nx, ny});
-                    area[nx][ny] = idx;
-                    visited[nx][ny] = true;
-                    size++;
+                    area[nx][ny] = area_idx;
+                    width++;
                 }
-            } 
+            }
         }
-        return size;
+        return width;
     }
 }
