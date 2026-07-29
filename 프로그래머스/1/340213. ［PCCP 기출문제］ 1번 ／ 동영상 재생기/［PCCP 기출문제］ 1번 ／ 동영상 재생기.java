270784d6a7ebf776391b5d6ca1911da4f2abd4aa
San <goodsan0330@gmail.com>
2026-07-29 21:51:01 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 10.74 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -1,71 +1,48 @@
+import java.util.*;
+
 class Solution {
+    
     /*
-     * 동영상 재생기 3가지 기능
-     * 1. 10초 전으로 이동: 'prev' 명령어 입력 -> Math.max(10초 전, 0분 0초)
-     * 2. 10초 후로 이동: 'next' 명령어 입력 -> Math.min(10초 후, 동영상 길이)
-     * 3. 오프닝 건너뛰기: 현재 재생 위치가 오프닝 구간인 경우 자동으로 오프닝 끝나는 위치로 이동
-     *
-     * video_len = 동영상 길이
-     * pos = 기능이 수행되기 직전의 재생 위치
-     * op_start = 오프닝 시작 시각
-     * op_end = 오프닝 끝나는 시각
+     * 동영상 재생기 기능 3가지
+     * 1. 10초 전 이동('prev') : pos = Math.max(0, now-10s)
+     * 2. 10초 후 이동('next') : pos = Math.min(동영상 길이, now+10s)
+     * 3. 오프닝 건너뛰기 : op_start <= 현재 재생 위치 <= op_end이면 자동으로 오프닝 끝나는 위치로 이동
     */
     
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
-        String[] times = pos.split(":");
-        int pos_mm = Integer.parseInt(times[0]); // 현재 분
-        int pos_ss = Integer.parseInt(times[1]); // 현재 초
+        int video_sec = parseSeconds(video_len);
+        int op_start_sec = parseSeconds(op_start);
+        int op_end_sec = parseSeconds(op_end);
+        int now_sec = parseSeconds(pos);
         
-        String now = pos; // 현재 시각
-        now = passOpening(now, op_start, op_end);
-        System.out.println(now);
+        if(op_start_sec <= now_sec && now_sec <= op_end_sec) {
+            now_sec = op_end_sec;
+        }
         
-        for(String c : commands) {
-            if(c.equals("prev")) {
-                now = calculateTime(now, -10, video_len);
-            } else if(c.equals("next")) {
-                now = calculateTime(now, 10, video_len);
+        for(String com : commands) {
+            if(com.equals("prev")) { // -10
+                now_sec = Math.max(0, now_sec-10);
+            } else { // +10
+                now_sec = Math.min(video_sec, now_sec+10);
             }
             
-            now = passOpening(now, op_start, op_end);
-            System.out.println(now);
+            if(op_start_sec <= now_sec && now_sec <= op_end_sec) {
+                now_sec = op_end_sec;
+            }
         }
         
-        return now;
+        return parseString(now_sec);
     }
     
-    private String passOpening(String now, String op_start, String op_end) {
-        String[] times = now.split(":");
-        int n = Integer.parseInt(times[0]) * 60 + Integer.parseInt(times[1]);
-        
-        String[] start = op_start.split(":");
-        int start_time = Integer.parseInt(start[0]) * 60 + Integer.parseInt(start[1]);
-        
-        String[] end = op_end.split(":");
-        int end_time = Integer.parseInt(end[0]) * 60 + Integer.parseInt(end[1]);
-        
-        if(start_time <= n && n <= end_time) {
-            return op_end;
-        }
-        
-        return now;
+    private int parseSeconds(String time) {        
+        String[] token = time.split(":");
+        return Integer.parseInt(token[0]) * 60 + Integer.parseInt(token[1]);
     }
     
-    private String calculateTime(String now, int jump_ss, String video_len) {
-        // now를 ss만큼 이동 (-10 or 10)
-        // 1. 10초 전으로 이동: 'prev' 명령어 입력 -> Math.max(10초 전, 0분 0초)
-        // 2. 10초 후로 이동: 'next' 명령어 입력 -> Math.min(10초 후, 동영상 길이)
-        String[] times = now.split(":");
-        int mm = Integer.parseInt(times[0]);
-        int ss = Integer.parseInt(times[1]);
+    private String parseString(int seconds) {
+        int mm = seconds / 60;
+        int ss = seconds % 60;
         
-        String[] video = video_len.split(":");
-        int video_mm = Integer.parseInt(video[0]);
-        int video_ss = Integer.parseInt(video[1]);
-        
-        int result = Math.min(Math.max(mm * 60 + ss + jump_ss, 0), video_mm * 60 + video_ss);
-        int result_mm = result / 60;
-        int result_ss = result % 60;
-        return String.format("%02d:%02d", result_mm, result_ss);
+        return String.format("%02d:%02d", mm, ss);
     }
 }
